a625ab8bacfdac698aa998441a6e2d04dfb57fac
Adrionor <jorgeadrian.dp@gmail.com>
2023-08-12 15:04:16 -0700

    commit corregido

--- activity/calculator/tests/unit/test_divide.py
+++ activity/calculator/tests/unit/test_divide.py
@@ -1,21 +1,25 @@
-# IMPORT the `operations` module
+import operations as c
 
-def test_divide():
+def test_divide(num1, num2):
     # Fraction
     # 10/2 = 5
-    # Insert your code here
-    pass
+    result1 = c.divide(num1, num2)
+    print(f"Expected 5 ... got {result1}")
 
+test_divide(10, 2)
 
-def test_divide_2():
+def test_divide_2(num1,num2):
     # Strings and divisions
     # "-15/4" / "1/2" =  -7.5 
-    # Insert your code here
-    pass
+    result2 = c.divide(num1, num2)
+    print(f"Expected -7.5 ... got {result2}")
 
+test_divide_2("-15/4", "1/2")
 
-def test_divide_3():
-    # Numbers as strings
+def test_divide_3(num1,num2):
+    # Strings and divisions
     # "8"/"16" = 0.5
-    # Insert your code here
-    pass
+    result3 = c.divide(num1, num2)
+    print(f"Expected 0.5 ... got {result3}")
+
+test_divide_3("8", "16")
