5d7520e1ea77859e5b7d4297d18e756f79e124ec
Adrionor <jorgeadrian.dp@gmail.com>
2023-08-12 18:28:08 -0700

    fix assert

--- activity/calculator/tests/unit/test_divide.py
+++ activity/calculator/tests/unit/test_divide.py
@@ -1,25 +1,28 @@
-import operations as c
+from .operations import divide
 
-def test_divide(num1, num2):
+def test_divide():
     # Fraction
     # 10/2 = 5
-    result1 = c.divide(num1, num2)
-    print(f"Expected 5 ... got {result1}")
+    result1 = divide(10,2)
+    #print(f"Expected 5 ... got {result1}")
+    assert result1 == 5
 
-test_divide(10, 2)
+#test_divide(10, 2)
 
-def test_divide_2(num1,num2):
+def test_divide_2():
     # Strings and divisions
     # "-15/4" / "1/2" =  -7.5 
-    result2 = c.divide(num1, num2)
-    print(f"Expected -7.5 ... got {result2}")
+    result2 = divide("-15/4", "1/2")
+    #print(f"Expected -7.5 ... got {result2}")
+    assert result2 == -7.5
 
-test_divide_2("-15/4", "1/2")
+#test_divide_2("-15/4", "1/2")
 
-def test_divide_3(num1,num2):
+def test_divide_3():
     # Strings and divisions
     # "8"/"16" = 0.5
-    result3 = c.divide(num1, num2)
-    print(f"Expected 0.5 ... got {result3}")
+    result3 = divide("8", "16")
+    #print(f"Expected 0.5 ... got {result3}")
+    assert result3 == 0.5
 
-test_divide_3("8", "16")
+#test_divide_3("8", "16")
--- activity/calculator/tests/unit/test_multiply.py
+++ activity/calculator/tests/unit/test_multiply.py
@@ -1,28 +1,31 @@
-import operations as c
+from .operations import multiply
 
-def test_multiply(num1,num2):
+def test_multiply():
     # Negative numbers
     # "-2" * "3" = -6
     # Insert your code here
-    result_m1 = c.multiply(num1, num2)
-    print(f"Expected -6 ... got {result_m1}")
+    result_m1 = multiply(-2, 3)
+    #print(f"Expected -6 ... got {result_m1}")
+    assert result_m1 == -6
 
-test_multiply (-2,3)
+#test_multiply (-2,3)
 
-def test_multiply_2(num1,num2):
+def test_multiply_2():
     # Fractions
     # "1/2" * "8/4" = 1
     # Insert your code here
-    result_m2 = c.multiply(num1, num2)
+    result_m2 = multiply(1/2, 8/4)
     print(f"Expected 1 ... got {result_m2}")
+    assert result_m2 == 1
 
-test_multiply_2("1/2","8/4")
+#test_multiply_2("1/2","8/4")
 
-def test_multiply_3(num1,num2):
+def test_multiply_3():
     # Positive numbers
     # "10" * "5" = 50
     # Insert your code here
-    result_m3 = c.multiply(num1, num2)
+    result_m3 = c.multiply(10,5)
     print(f"Expected 50 ... got {result_m3}")
+    assert result_m3 == 50
 
-test_multiply_3("10","5")
+#test_multiply_3("10","5")
